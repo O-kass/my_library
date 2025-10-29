c34251144bf497f10f3697d2efe7410e7a3e4285
Omar Kassam <omar.kassam@kcl.ac.uk>
2025-10-29 10:33:06 +0000

    add url for books in navbar dropdown

--- my_library/settings.py
+++ my_library/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = ['okass.pythonanywhere.com',
+                 '127.0.0.1',
+                 ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -102,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-ALLOWED_HOSTS = ['okass.pythonanywhere.com']
+
 
 
 # Internationalization
